24af28405e308e143a1d3a8f6c0fbbad2ffe3331
Tretboot <37165913+Tretboot@users.noreply.github.com>
2023-04-13 03:43:43 +0200

    Update BingCompose.py
    cleaned up some code (there is still spelling errors and stuff in there)

--- BingCompose.py
+++ BingCompose.py
@@ -5,60 +5,60 @@
 # Es kann die Anzahl der generierten Zeilen einstellen
 
 # Importiere die benötigten Module
-importiere random
-importiere tkinter
-importiere math
+import random
+import tkinter
+import math
 
 # Definiere eine Funktion, die einen zufälligen Pseudocode-Befehl zurückgibt
-definiere zufalls_befehl():
+def zufalls_befehl():
   # Wähle eine zufällige Zahl zwischen 1 und 10 aus
   zahl = random.randint(1, 10)
-  # Wenn die Zahl 1 ist, gib eine Zuweisung zurück
-  wenn zahl == 1:
+  # if die Zahl 1 ist, gib eine Zuweisung zurück
+  if zahl == 1:
     # Wähle einen zufälligen Variablennamen aus
     name = random.choice(["x", "y", "z", "a", "b", "c"])
     # Wähle einen zufälligen Wert aus
     wert = random.choice([0, 1, 2, 3, "wahr", "falsch", "nichts"])
     # Gib die Zuweisung zurück
-    gib_zurück name + " = " + str(wert)
-  # Wenn die Zahl 2 ist, gib eine Schleife zurück
-  wenn zahl == 2:
+    return name + " = " + str(wert)
+  # if die Zahl 2 ist, gib eine Schleife zurück
+  if zahl == 2:
     # Wähle einen zufälligen Schleifentyp aus
-    typ = random.choice(["für", "solange"])
-    # Wenn der Typ für ist, gib eine für-Schleife zurück
-    wenn typ == "für":
+    typ = random.choice(["for", "solange"])
+    # if der Typ for ist, gib eine for-Schleife zurück
+    if typ == "for":
       # Wähle einen zufälligen Variablennamen aus
       name = random.choice(["i", "j", "k", "n", "m"])
       # Wähle einen zufälligen Startwert aus
       start = random.randint(0, 10)
       # Wähle einen zufälligen Endwert aus
       ende = random.randint(start + 1, start + 10)
-      # Gib die für-Schleife zurück
-      gib_zurück "für " + name + " von " + str(start) + " bis " + str(ende) + ":"
-    # Wenn der Typ solange ist, gib eine solange-Schleife zurück
-    wenn typ == "solange":
+      # Gib die for-Schleife zurück
+      return "for " + name + " von " + str(start) + " bis " + str(ende) + ":"
+    # if der Typ solange ist, gib eine solange-Schleife zurück
+    if typ == "solange":
       # Wähle eine zufällige Bedingung aus
       bedingung = random.choice(["x < y", "y > z", "z == a", "a != b", "b < c", "c > x"])
       # Gib die solange-Schleife zurück
-      gib_zurück "solange " + bedingung + ":"
-  # Wenn die Zahl 3 ist, gib eine Bedingung zurück
-  wenn zahl == 3:
+      return "solange " + bedingung + ":"
+  # if die Zahl 3 ist, gib eine Bedingung zurück
+  if zahl == 3:
     # Wähle eine zufällige Bedingung aus
     bedingung = random.choice(["x < y", "y > z", "z == a", "a != b", "b < c", "c > x"])
     # Gib die Bedingung zurück
-    gib_zurück "wenn " + bedingung + ":"
-  # Wenn die Zahl 4 ist, gib eine Funktion zurück
-  wenn zahl == 4:
+    return "if " + bedingung + ":"
+  # if die Zahl 4 ist, gib eine Funktion zurück
+  if zahl == 4:
     # Wähle einen zufälligen Funktionsnamen aus
     name = random.choice(["funktion1", "funktion2", "funktion3", "funktion4"])
     # Wähle eine zufällige Anzahl von Parametern aus
     anzahl = random.randint(0, 3)
     # Erstelle eine leere Liste von Parameternamen
     parameter = []
-    # Für jede Anzahl von Parametern:
-    für i von 0 bis anzahl:
+    # for jede Anzahl von Parametern:
+    for i in parameter:
       # Wähle einen zufälligen Parameternamen aus und füge ihn der Liste hinzu
-      parameter.append(random.choice(["p", "q", "r", "s"]))
+       parameter.append(random.choice(["p", "q", "r", "s"]))
     # Erstelle einen String aus den Parameternamen mit Kommas getrennt
     parameter_string = ", ".join(parameter)
     # Gib dieligen Variablennamen aus
@@ -66,68 +66,68 @@ definiere zufalls_befehl():
     # Wähle eine zufällige Anzahl von Iterationen aus
     anzahl = random.randint(1, 10)
     # Gib die Schleife zurück
-    gib_zurück "für " + name + " in Bereich(" + str(anzahl) + "):"
-  # Wenn die Zahl 3 ist, gib eine Bedingung zurück
-  wenn zahl == 3:
+    return "for " + name + " in range(" + str(anzahl) + "):"
+  # if die Zahl 3 ist, gib eine Bedingung zurück
+  if zahl == 3:
     # Wähle einen zufälligen Ausdruck aus
     ausdruck = random.choice(["x > y", "y < z", "z == a", "a != b", "b und c", "c oder x"])
     # Gib die Bedingung zurück
-    gib_zurück "wenn " + ausdruck + ":"
-  # Wenn die Zahl 4 ist, gib eine Funktion zurück
-  wenn zahl == 4:
+    return "if " + ausdruck + ":"
+  # if die Zahl 4 ist, gib eine Funktion zurück
+  if zahl == 4:
     # Wähle einen zufälligen Funktionsnamen aus
     name = random.choice(["funktion1", "funktion2", "funktion3", "funktion4", "funktion5"])
     # Wähle eine zufällige Anzahl von Parametern aus
     anzahl = random.randint(0, 3)
     # Erstelle eine Liste von Parameternamen
     parameter = []
-    für i in Bereich(anzahl):
+    for i in range(anzahl):
       # Wähle einen zufälligen Parameternamen aus
       parameter.append(random.choice(["p", "q", "r", "s", "t"]))
     # Gib die Funktion zurück
-    gib_zurück "definiere " + name + "(" + ", ".join(parameter) + "):"
-  # Wenn die Zahl 5 ist, gib einen Funktionsaufruf zurück
-  wenn zahl == 5:
+    return "definiere " + name + "(" + ", ".join(parameter) + "):"
+  # if die Zahl 5 ist, gib einen Funktionsaufruf zurück
+  if zahl == 5:
     # Wähle einen zufälligen Funktionsnamen aus
     name = random.choice(["funktion1", "funktion2", "funktion3", "funktion4", "funktion5"])
     # Wähle eine zufällige Anzahl von Argumenten aus
     anzahl = random.randint(0, 3)
     # Erstelle eine Liste von Argumenten
     argumente = []
-    für i in Bereich(anzahl):
+    for i in range(anzahl):
       # Wähle ein zufälliges Argument aus
       argumente.append(random.choice([0, 1, 2, 3, "wahr", "falsch", "nichts"]))
     # Gib den Funktionsaufruf zurück
-    gib_zurück name + "(" + ", ".join(str(arg) for arg in argumente) + ")"
-  # Wenn die Zahl 6 ist, gib einen Kommentar zurück
-  wenn zahl == 6:
+    return name + "(" + ", ".join(str(arg) for arg in argumente) + ")"
+  # if die Zahl 6 ist, gib einen Kommentar zurück
+  if zahl == 6:
     # Gib den Kommentar zurück
-    gib_zurück "# Ich habe keine Ahnung, was diese Funktion macht"
-  # Wenn die Zahl 7 ist, gib eine leere Zeile zurück
-  wenn zahl == 7:
+    return "# Ich habe keine Ahnung, was diese Funktion macht"
+  # if die Zahl 7 ist, gib eine leere Zeile zurück
+  if zahl == 7:
     # Gib die leere Zeile zurück
-    gib_zurück ""
-  # Wenn die Zahl 8 ist, gib eine Einrückung zurück
-  wenn zahl == 8:
+    return ""
+  # if die Zahl 8 ist, gib eine Einrückung zurück
+  if zahl == 8:
     # Gib die Einrückung zurück
-    gib_zurück "\t"
-  # Wenn die Zahl 9 ist, gib eine Ausrückung zurück
-  wenn zahl == 9:
+    return "\t"
+  # if die Zahl 9 ist, gib eine Ausrückung zurück
+  if zahl == 9:
     # Gib die Ausrückung zurück
-    gib_zurück "\b"
-  # Wenn die Zahl 10 ist, gib eine Rückkehr zurück
-  wenn zahl == 10:
+    return "\b"
+  # if die Zahl 10 ist, gib eine Rückkehr zurück
+  if zahl == 10:
     # Gib die Rückkehr zurück
-    gib_zurück "\n"
+    return "\n"
 
 # Definiere eine Funktion, die einen Pseudocode in Mandelbrot Fraktalform generiert
-definiere mandelbrot_pseudocode():
+def mandelbrot_pseudocode():
   # Erstelle eine leere Liste von Zeilen
   zeilen = []
-  # Erstelle eine Variable für die aktuelle Einrückungsstufe
+  # Erstelle eine Variable for die aktuelle Einrückungsstufe
   einrückung = ""
-  # Erstelle eine Variable für die maximale Anzahl von Zeilen
-  max_zeilen = int(eingabe("Wie viele Zeilen möchtest du generieren? "))
+  # Erstelle eine Variable for die maximale Anzahl von Zeilen
+  max_zeilen = int(input("Wie viele Zeilen möchtest du generieren? "))
   
 # Erstelle ein tkinter-Fenster
 fenster = tkinter.Tk()
@@ -144,7 +144,7 @@ text.pack(expand=True, fill="both")
 text.config(font=("Courier", 12))
 
 # Definiere eine Funktion, die den Pseudocode in der Textbox kopiert
-definiere kopiere_code():
+def kopiere_code():
   # Hole den gesamten Text aus der Textbox
   code = text.get("1.0", "end")
   # Kopiere den Text in die Zwischenablage
@@ -152,39 +152,39 @@ definiere kopiere_code():
   fenster.clipboard_append(code)
 
 # Definiere eine Funktion, die neuen Pseudocode in der Textbox generiert
-definiere generiere_code():
+def generiere_code():
   # Lösche den alten Text aus der Textbox
   text.delete("1.0", "end")
-  # Setze eine Variable für die Anzahl der Zeilen
+  # Setze eine Variable for die Anzahl der Zeilen
   zeilen = 10
-  # Setze eine Variable für die Einrückungsebene
+  # Setze eine Variable for die Einrückungsebene
   ebene = 0
-  # Wiederhole für jede Zeile
-  für i von 0 bis zeilen:
+  # Wiederhole for jede Zeile
+  for i in  range zeilen:
     # Generiere einen zufälligen Befehl
     befehl = zufalls_befehl()
     # Füge Leerzeichen entsprechend der Einrückungsebene hinzu
     befehl = " " * (ebene * 4) + befehl
     # Füge den Befehl in die Textbox ein
     text.insert("end", befehl + "\n")
-    # Wenn der Befehl eine Schleife ist, erhöhe die Einrückungsebene um 1
-    wenn befehl.startswith("für") oder befehl.startswith("solange"):
+    # if der Befehl eine Schleife ist, erhöhe die Einrückungsebene um 1
+    if befehl.startswith("for") or befehl.startswith("solange"):
       ebene = ebene + 1
-    # Wenn die Einrückungsebene größer als 0 ist, verringere sie zufällig um 1 mit einer Wahrscheinlichkeit von 0.5
-    wenn ebene > 0 und random.random() < 0.5:
+    # if die Einrückungsebene größer als 0 ist, verringere sie zufällig um 1 mit einer Wahrscheinlichkeit von 0.5
+    if ebene > 0 and random.random() < 0.5:
       ebene = ebene - 1
 
 # Definiere eine Funktion, die den Pseudocode in der Textbox degeneriert
-definiere degeneriere_code():
+def degeneriere_code():
   # Hole den gesamten Text aus der Textbox
   code = text.get("1.0", "end")
   # Ersetze alle Buchstaben durch zufällige Zeichen aus dem Mandelbrot Fraktalset
   zeichen = [" ", ".", ",", "-", "+", "*", "/", "%", "^", "(", ")", "[", "]", "{", "}"]
   neuer_code = ""
-  für c in code:
-    wenn c.isalpha():
+  for c in code:
+    if c.isalpha():
       neuer_code = neuer_code + random.choice(zeichen)
-    sonst:
+    else:
       neuer_code = neuer_code + c
   # Lösche den alten Text aus der Textbox
   text.delete("1.0", "end")
